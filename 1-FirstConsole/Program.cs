4b174bb94be612948d54a370245ca84d9e340fc3
Veysel ÖREN <orenveysel@outlook.com>
2024-03-28 18:09:43 +0300

    Add project files.

---
+++ 1-FirstConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _1_FirstConsole
+{
+    internal class Program
+    {
+        /*
+         * 
+         * Main metodu olmazsa program calismaz
+         * Isletim sistemleri calistirilacak programda mutlaka main metodunu arar
+         * 
+         */
+        static void Main(string[] args)
+        {
+            // Bu bir yorum satiridir
+            Console.BackgroundColor = ConsoleColor.Magenta;
+            Console.ForegroundColor = ConsoleColor.Yellow;  
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
