346ba1e908e5a4105cfc72bb6c3d2f5c7825c4a5
Veysel ÖREN <orenveysel@outlook.com>
2024-04-01 12:34:00 +0300

    Degisken Tipleri, Scope Kavrami, var keyword

--- 1-FirstConsole/Program.cs
+++ 1-FirstConsole/Program.cs
@@ -68,7 +68,7 @@
             */
             #endregion
 
-            //----------------------------------------
+            //------------------------------------------------------------------------------------------------------------------
 
             #region Degisken Tanimlama Kurallari
             /*
@@ -80,19 +80,165 @@
 
             */
             #endregion
-            /*
-            string ad, soyad, tcNo;
-            Console.WriteLine("Adınızı giriniz: ");
-            ad = Console.ReadLine();
-            Console.WriteLine("Soyadınızı giriniz: ");
-            soyad = Console.ReadLine();
-            Console.WriteLine("Tc kimlik numaranızı giriniz: ");
-            tcNo = Console.ReadLine();
 
-            Console.WriteLine("Bilgileri girilen kişinin adı: " + ad + ", soyadı: " + soyad + ", Tc kimlik numarası: " + tcNo);
-            Console.ReadLine();
+            //------------------------------------------------------------------------------------------------------------------
+
+            #region İki sayinin toplami
+            /*
+            // Asagidaki degisken tanimlamalari aynidir
+            string input1, input2;
+            int sayi1 = 0;
+            Int32 sayi2 = 0; // kisa keyword int
+
+            Int64 sayi3 = 0; // kisa keyword long
+            long sayi4 = 0;
+
+            Console.WriteLine("Birinci Sayiyi Giriniz: ");
+            input1 = Console.ReadLine();
+            // Girilen ifadeyi int veri tipine çevirmek icin
+            // 1. yol
+
+            sayi1 = Convert.ToInt32(input1);
+            Console.WriteLine("İkinci Sayiyi Giriniz: ");
+            input2 = Console.ReadLine();
+
+            // Girilen ifadeyi int veri tipine çevirmek icin
+            // 2. yol
+            sayi2 = int.Parse(input2);
+
+            Console.WriteLine("Girilen sayilarin toplami: " + (sayi1 + sayi2)); // Burasi normal toplama yapar
+            Console.WriteLine("Girilen sayilarin toplami: " + (input1 + input2)); // Burasi string birlestirme islemi yapar
             */
 
+            #endregion
+
+            //------------------------------------------------------------------------------------------------------------------
+
+            #region Kesirli Sayilar
+
+            /* Kesirli Sayilar
+            
+            float : Uzunluğu 4 byte'dir, 1.5 x 10-45 ile 3.4 x 1038 arasında değer alır.
+
+            double : Uzunluğu 8 byte'dir, 5.0 x 10-324 ile 1.7 x 10308 arasında değer alır.
+
+            decimal : Uzunluğu 12 byte'dir, ±1.0 x 10-28 ile ±7.9 x 1028 arasında değer alır.
+
+            Asagıdaki tanimlama hata verecektir
+            float sayiFloat = 12.5;
+            decimal sayiDecimal = 12.5;
+
+            Eger noktali bir sayi atamasi yapiliyorsa default olarak c# double kabul eder
+            double sayiDouble = 12.5;
+
+            Duzeltilmis hali
+            float sayiFloat = 12.5f;
+            decimal sayiDecimal = 12.5m;
+
+            */
+
+            #endregion
+
+            //------------------------------------------------------------------------------------------------------------------
+
+            #region String Degiskenler
+            /*
+            
+            string  => kelime ya da cumle, paragraf artik ne atadiysaniz kabul eder
+
+            char => uzunlugu 2 byte'dir. Unicode karakter temsil eder. Bir karakter kabul eder
+
+            string str = "asdk asldkjasbd alsjdnasjnd .askjdnaskjd";
+            char c = 'a';
+
+            */
+
+            #endregion
+
+            //------------------------------------------------------------------------------------------------------------------
+
+            #region Mantiksal Degisken
+            /*
+            
+            bool veri tipi true ya da false degerleri alir
+            bool devamMi = false;
+
+            */
+            #endregion
+
+            //------------------------------------------------------------------------------------------------------------------
+
+            #region null ifadesi ve nullable degiskenler
+            /*
+            null ascii tablodaki 0. elemandır. Ozel bir karakterdir. Hiclik anlamina gelir
+
+            int a = null; 
+            bu satir hata verecektir, nullable degildir cunku
+
+            herhangi bir veri tipinin sonuna ? eklenirse nullable oldugu anlamina gelir
+            int? b = null;
+
+            */
+
+            #endregion
+
+            //------------------------------------------------------------------------------------------------------------------
+
+            #region var keyword
+            /*
+            
+            Degisken tanimlamalarinda once tip sonra degisken adi verilmek zorundadir
+            var ile kullanilir ise atanan deger ne ise degisken o veri tipine burunur
+
+            var ali = "Ali veli";
+            var sayi = 12.5;
+
+            */
+            #endregion
+
+            //------------------------------------------------------------------------------------------------------------------
+
+            #region Tarih ve Zaman
+            /*
+            Tarih ve zamani beraber tutar
+            DateTime dateTime = DateTime.Now;
+
+            Kendi belirledigimiz tarihi olusturma
+            DateTime tarih = new DateTime(2000, 2, 2, 12, 15, 30);
+            Console.WriteLine(dateTime);
+
+            DateOnly Sadece tarihi tutar, zamani tutmaz
+            DateOnly trh = new DateOnly(2000,2,2);
+
+            Sadece zamani tutar
+            TimeOnly trh2 = new TimeOnly(12, 12, 12, 12);
+
+            */
+            #endregion
+
+            //------------------------------------------------------------------------------------------------------------------
+
+            #region Degiskenlerde Scope Kavrami
+            /*
+            string cumle = "Deneme 123";
+            {
+                cumle = cumle + " alt scope";
+                int sayi3 = 0;
+                sayi3 = 5 * 4;
+                {
+                    Console.WriteLine(sayi3);
+                    int sayi4 = 0;
+                }
+                // Burasi da hata verecektir, sayi3 ust scope icerisinde kaldi
+                Console.WriteLine(sayi4);
+            }
+            // Burasi hata verecektir cunku yukaridaki scope icerisinde kaldi ve biz sayi3 degiskenine ulasamıyoruz
+            Console.WriteLine(sayi3);
+            */
+            #endregion
+
+
+            
         }
     }
 }
