e1581bba9722fe04a02b325033eed4d3fa759448
Veysel ÖREN <orenveysel@outlook.com>
2024-04-11 11:37:32 +0300

    Try Catch

--- 6-Try-Catch/Form1.Designer.cs
+++ 6-Try-Catch/Form1.Designer.cs
@@ -28,34 +28,94 @@
         /// </summary>
         private void InitializeComponent()
         {
-            Onaylama = new Button();
             textBox1 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
             SuspendLayout();
-            // 
-            // Onaylama
-            // 
-            Onaylama.Location = new Point(153, 214);
-            Onaylama.Name = "Onaylama";
-            Onaylama.Size = new Size(188, 58);
-            Onaylama.TabIndex = 0;
-            Onaylama.Text = "Buton";
-            Onaylama.UseVisualStyleBackColor = true;
-            Onaylama.Click += Onaylama_Click;
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(211, 72);
+            textBox1.Location = new Point(239, 83);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(112, 47);
+            textBox1.Size = new Size(263, 47);
             textBox1.TabIndex = 1;
+            //
+            // button1
+            // 
+            button1.Location = new Point(274, 167);
+            button1.Name = "button1";
+            button1.Size = new Size(188, 58);
+            button1.TabIndex = 0;
+            button1.Text = "Onayla";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;            
+            // 
+            // button2
+            // 
+            button2.Location = new Point(251, 256);
+            button2.Name = "button2";
+            button2.Size = new Size(406, 58);
+            button2.TabIndex = 2;
+            button2.Text = "Hata Yonetimiyle Onayla";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(297, 349);
+            button3.Name = "button3";
+            button3.Size = new Size(188, 58);
+            button3.TabIndex = 3;
+            button3.Text = "Coklu Catch Kullanimi";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(297, 441);
+            button4.Name = "button4";
+            button4.Size = new Size(188, 58);
+            button4.TabIndex = 4;
+            button4.Text = "Coklu Catch Kullanimi 2";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(286, 536);
+            button5.Name = "button5";
+            button5.Size = new Size(188, 58);
+            button5.TabIndex = 5;
+            button5.Text = "Hata Firlatma";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(274, 640);
+            button6.Name = "button6";
+            button6.Size = new Size(188, 58);
+            button6.TabIndex = 6;
+            button6.Text = "Finnaly";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(17F, 41F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1010, 982);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1); 
             Controls.Add(textBox1);
-            Controls.Add(Onaylama);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
@@ -65,7 +125,12 @@
 
         #endregion
 
-        private Button Onaylama;
-        private TextBox textBox1;
+        private TextBox textBox1;        
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
     }
 }
--- 6-Try-Catch/Form1.cs
+++ 6-Try-Catch/Form1.cs
@@ -1,5 +1,30 @@
 namespace _6_Try_Catch
 {
+    /*
+          * ISTISNA TIPLERI
+          * 
+          * Calisma Zamani Hatalarý (Runtime Exception) => Cogunlukla kullanici kaynakli hatalardir. (Verinin yanlis, eksik ya da bos girilmesi). Cozum Yolu, try-catch ile hatalari ele alabilirsiniz...
+          * 
+          * Derleme Zamani Hatalari (Compile Exception) => Tamamen yazilimci kaynakli hatalardir. Yazim hatalari meydana geldiginde devreye girer. En kolay cozulen hata tipidir. Visual Studio sizi uyarir (asagidaki Error List penceresi ile). Ilgili hataya cift tiklayip detaya gidebilir ve hatayi aninda duzeltebilirsiniz...
+          * 
+          * Mantiksal Hatalar => Tamamen programci kaynakli hatalardir. Uygulamanin algoritmasinda meydana gelir. Ornegin bolme yapilacak yerde islem unutulmasi, degerin yanlis bir degerle degistirilmesi... Cozulmesi en zor hatalardir. Anlik olarak derleme asamasini takip etmeli ve degerlerinizi gozlemlemelisiniz. Bu konuda Visual Studio size sagladigi tool'larla yardimci olur. (Immediate Window, Watch, Breakpoint)
+          */
+    /*
+    * BREAKPOINT => Derleme asamasina adim adim dahil olmak icin kullandigimiz yardimci tool'dur. Yapmaniz gereken tek sey, hangi satirdan derleme anina dahil olmak istiyorsaniz, ilgili satirin en sol kismina gelip (gri alan) mouse'un sol tusuyla bir kýrmýzý top yerlestirmek. Artik derleyici bu satira geldigi zaman sizi olaya dahil edecektir...
+    * F11 ile adim ilerleyebilirsiniz. Degiskenlerin, nesnelerin uzerine gelip o anki degerlerini ogrenebilirsiniz, islemin bittigine inanýyorsaniz F5'e basip breakpoint'ten cikabilir ve islemin kendisini devam ettirmesini saglayabilirsiniz.
+    * Sol blokta cikan sari ok, derlenen satirin isaretcisidir. Mouse yardimiyla ileri ya da geri oynatabilir yani adimlari atlayabilir ya da tekrarlatabilirsiniz...
+    */
+
+    /*
+     * WATCH
+     * Yalnizca run-time'da - calisma zamani - kullanabilecegimiz bir tool'dur. Dolayisiyla pencereyi sadece calisma zamaninda gorursunuz...
+     * Ýlk ayarlarinizda Watch penceresi kapali olabilir. Uygulamanizi calistirip "CTRL+ALT+W" ardindan 1'e basarsaniz Watch penceresini getirirsiniz...
+     * Run-time'de elinizde olan degerleri (kontrol degerler, degisken degerleri) degistirmenize, izlemenize olanak saglayan yardimci tool'dur. Acilan penceredeki "Name" alanina ilgili degiskenin adini yazip Enter'a basarsaniz, "Value" alaninda o anki degerini gozlemleyebilirsiniz. Hatta degistirebilirsiniz...
+     * "Name" alanina degeri ilk yazmaya basladiginizda intellisense size destek vermez. Ancak "CTRL+Space" kombinasyonu ile intellisense'i aktif hale getirebilirsiniz. Hatta cogu zaman kendisi otomatik olarak tamamlar. 
+     * Noktalar, virgüller, noktali virgüller - yani tum yazim kurallari - watch penceresi icin de gecerlidir...
+     */
+
+    //Eger disaridan bir veri aliyorsaniz, verinin tutarligindan emin de degilseniz, orada mutlaka hata riski vardýr!
     public partial class Form1 : Form
     {
         public Form1()
@@ -9,19 +34,157 @@ namespace _6_Try_Catch
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            //try
+            //{
+            //    //Yapilacak iþler buraya yazilacak
+            //    string str = "ali";
+            //    int a = Convert.ToInt32(str);
+            //}
+            //catch (Exception)
+            //{
+            //    //Yakalanan hata olursa burada iþlem gorecek
+            //    throw new Exception(" Formun Yuklenmesi sirasinda hata olustu");
+            //}
+            //finally
+            //{
+            //    // Hata olsun yada olmasin burasi mutlaka calisacak.
+            //    // Genelde SqlConnection tarzi socket iþlemlerinde acik olan connection'i kapatmaya yarar.
+            //}
         }
 
-        private void Onaylama_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)
         {
             int a, b, c;
-            a= 5;
-            b= 6;
-            c= 7;
+            a = 5;
+            b = 6;
+            c = 7;
             int toplam = a + b + c; // 18 cikar
             int carpma = a * b * c; // 210 cikar
             c = 1;
-            int bolum = a / c; // DivideByZero hatasi alacaktir
+            int bolum = a / c;
+            int gelenDeger = Convert.ToInt32(textBox1.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int a, b, c;
+                a = 5;
+                b = 6;
+                c = 7;
+                int toplam = a + b + c; // 18 cikar
+                int carpma = a * b * c; // 210 cikar
+                c = 0;
+                int bolum = a / c; // DivideByZero hatasi alacaktir
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                throw;
+            }
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int a, b, c;
+                a = 5;
+                c = 7;
+
+                c = int.Parse(textBox1.Text);
+                int bolum = a / c;
+            }
+            catch (DivideByZeroException ex)
+            {
+                MessageBox.Show("Sifira Bolme Hatasi Olustu. Lutfen 0 Girmeyiniz");
+
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show("Lutfen Sayi Giriniz");
+
+            }
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int a, b, c;
+                a = 5;
+
+                c = int.Parse(textBox1.Text);
+                int bolum = a / c;
+            }
+            // Bu kisim mutlaka sonda olacak . Eger Diger Exception'larin onunde olursa hata alirsiniz.
+            //catch (Exception ex) // burasi mutlaka en son kisimda olmasi gerekiyor.
+            //{
+            //    MessageBox.Show("Beklenmedik bir hata olustu");
+            //}
+            catch (FormatException ex)
+            {
+                MessageBox.Show("Lutfen Sayi Giriniz");
+
+            }
+            catch (DivideByZeroException ex)
+            {
+                MessageBox.Show("Sifira Bolme Hatasi Olustu. Lutfen 0 Girmeyiniz");
+
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                int gelenDeger = Convert.ToInt32(textBox1.Text);
+                int sonuc = gelenDeger / 0;
+            }
+            catch (FormatException hata) // Format Hatasi
+            {
+
+                throw new Exception("Girilen deger integer tipinde degildir..");
+            }
+            catch (DivideByZeroException hata) // Sifira bolme Hatasi
+            {
+
+                throw new Exception("Sifira Bolme hatasi:" + hata.Source);
+            }
+            catch (OverflowException hata) // Taþma hatasi
+            {
+
+                throw new Exception(hata.Message);
+            }
+            catch (Exception hata) // Burasi gelen butun hatalari yakalar
+            {
+
+                throw new Exception(hata.Message);
+            }
+            finally // Burasi en son yapilmasi gereken isler icin kullanilir
+            {
+                MessageBox.Show("Her kosulda en son burasi calisir");
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int gelenDeger = Convert.ToInt32(textBox1.Text);
+                MessageBox.Show("Tebrikler Dogru Formatta girdiniz");
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Hata Olustu. Detay :" + ex.Message);
+            }
+            finally // Burasi en son yapilmasi gereken isler icin kullanilir
+            {
+                MessageBox.Show("Her kosulda en son burasi calisir");
+            }
         }
     }
 }
